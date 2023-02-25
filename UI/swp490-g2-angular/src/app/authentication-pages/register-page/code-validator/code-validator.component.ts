56b8d063811afe43d8e3a1cda4c8eb0c8b148a8b
baotq22 <87086420+baotq22@users.noreply.github.com>
2023-02-25 23:13:30 +0700

    WIP

--- UI/swp490-g2-angular/src/app/authentication-pages/register-page/code-validator/code-validator.component.ts
+++ UI/swp490-g2-angular/src/app/authentication-pages/register-page/code-validator/code-validator.component.ts
@@ -1,34 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { Client } from "src/app/ngswag/client";
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Client } from 'src/app/ngswag/client';
+import { CustomValidators } from 'src/app/utils';
 
 @Component({
   selector: 'app-code-validator',
   templateUrl: './code-validator.component.html',
-  styleUrls: ['./code-validator.component.scss']
+  styleUrls: ['./code-validator.component.scss'],
 })
-export class CodeValidatorComponent implements OnInit {
-
+export class CodeValidatorComponent implements OnInit, AfterViewInit {
+  @ViewChild('form', { static: false }) form!: NgForm;
   client = new Client();
   visible = true;
-  @Input() email = "longlunglay5@gmail.com";
-  code = "";
+  @Input() email = 'longlunglay5@gmail.com';
+  code = '';
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
     console.log(this.email);
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.form.controls['code'].addValidators([
+        Validators.required,
+        CustomValidators.patternValidator(/^[0-9]{1,6}$/, { hasNumber: true }),
+      ]);
+    }, 0);
+  }
+
   async submit() {
     try {
       await this.client.verifyCode(this.email, this.code);
-      console.log("Account registered successfully");
+      console.log('Account registered successfully');
     } catch (err) {
-      alert("Invalid code verification");
+      alert('Invalid code verification');
     } finally {
-      
     }
   }
 }
