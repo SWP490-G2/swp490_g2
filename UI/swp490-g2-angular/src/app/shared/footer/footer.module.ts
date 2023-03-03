1d5ed091275244595d7a37165e9afae05e341509
baotq22 <87086420+baotq22@users.noreply.github.com>
2023-03-03 19:07:01 +0700

    Merged

--- UI/swp490-g2-angular/src/app/shared/footer/footer.module.ts
+++ UI/swp490-g2-angular/src/app/shared/footer/footer.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
-
+import { DividerModule } from 'primeng/divider';
 
 
 @NgModule({
   declarations: [FooterComponent],
   imports: [
+    DividerModule,
     CommonModule
   ],
   exports: [FooterComponent]
