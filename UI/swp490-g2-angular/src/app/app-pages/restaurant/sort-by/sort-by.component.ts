567c032cfefb51ef4a9fa6ef2af74739820f32d7
toan15042000 <toanndhe140540@fpt.edu.vn>
2023-03-14 22:54:19 +0700

    WIP

---
+++ UI/swp490-g2-angular/src/app/app-pages/restaurant/sort-by/sort-by.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-sort-by",
+  templateUrl: "./sort-by.component.html",
+  styleUrls: ["./sort-by.component.scss"],
+})
+export class SortByComponent {
+  animal = 1;
+
+  tier1 = 1;
+
+  tier2 = 1;
+
+  hotel = 1;
+
+  subscription = 1;
+
+  payment = 1;
+}
