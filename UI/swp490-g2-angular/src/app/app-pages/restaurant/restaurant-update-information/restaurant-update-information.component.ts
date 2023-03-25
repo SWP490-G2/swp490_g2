fbcca0323bd8a13fea8600be2832f083f889e631
Tungndhe141593 <tungndhe141593@fpt.edu.vn>
2023-03-25 20:03:36 +0700

    Finish basic information update

--- UI/swp490-g2-angular/src/app/app-pages/restaurant/restaurant-update-information/restaurant-update-information.component.ts
+++ UI/swp490-g2-angular/src/app/app-pages/restaurant/restaurant-update-information/restaurant-update-information.component.ts
@@ -1,20 +1,27 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { finalize } from 'rxjs';
-import { Restaurant, RestaurantClient } from 'src/app/ngswag/client';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { finalize } from "rxjs";
+import { Restaurant, RestaurantClient, Ward } from "src/app/ngswag/client";
 
 @Component({
-  selector: 'app-restaurant-update-information',
-  templateUrl: './restaurant-update-information.component.html',
-  styleUrls: ['./restaurant-update-information.component.scss']
+  selector: "app-restaurant-update-information",
+  templateUrl: "./restaurant-update-information.component.html",
+  styleUrls: ["./restaurant-update-information.component.scss"],
 })
 export class RestaurantUpdateInformationComponent {
   @Input() restaurant: Restaurant;
   @Input() editable: boolean;
   @ViewChild("form", { static: false }) form!: NgForm;
+  @Output() onHide = new EventEmitter();
 
-  displayModal: boolean = false;
+  displayModal = false;
   private _submitButtonDisabled = false;
 
   constructor(
@@ -32,12 +39,24 @@ export class RestaurantUpdateInformationComponent {
   }
 
   submit(): void {
+    if (this.restaurant.address) {
+      this.restaurant.address.ward = new Ward({
+        id: this.form.value.ward.id,
+      });
+
+      this.restaurant.address.specificAddress = this.form.value.specificAddress;
+    }
+
     this.$restaurantClient.update(this.restaurant).subscribe(() => {
       this.$message.add({
         severity: "success",
         summary: "Success",
-        detail: `Restaurant's information has changed`,
+        detail: "Restaurant's information has changed",
       });
     });
+  }
+
+  onDialogHide() {
+    this.onHide.emit();
   }
 }
