f10b0d8f684fe52cd3b11410f1b9e40d05d19762
WINDOWS-10\Administrator <khanhnqhe141470@fpt.edu.vn>
2023-04-13 21:59:59 +0700

    regex phone number

--- UI/swp490-g2-angular/src/app/authentication-pages/register-page/register.component.ts
+++ UI/swp490-g2-angular/src/app/authentication-pages/register-page/register.component.ts
@@ -43,7 +43,7 @@ export class RegisterComponent implements OnInit, AfterViewInit {
 
       this.form.controls["phoneNumber"].addValidators([
         Validators.required,
-        Validators.pattern("^(0[3|5|7|8|9])+([0-9]{8})$"),
+        Validators.pattern("^(([+]84)|0)|([3|5|7|8|9])+([0-9]{8})$"),
       ]);
       this.form.controls["phoneNumber"].updateValueAndValidity();
     }, 0);
