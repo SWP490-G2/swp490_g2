ba941e782de7c4828aa887f318d528fe93fbfda3
Tungndhe141593 <tungndhe141593@fpt.edu.vn>
2023-04-14 17:32:02 +0700

    navigate to homepage when resgister successfully

--- SpringBootAPI/HRMS_SWP490_G2/src/main/java/com/swp490_g2/hrms/security/AuthenticationRequest.java
+++ SpringBootAPI/HRMS_SWP490_G2/src/main/java/com/swp490_g2/hrms/security/AuthenticationRequest.java
@@ -11,5 +11,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequest {
     private String emailOrPhoneNumber;
-    String password;
+    private String password;
 }
