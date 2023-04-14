24924e128e857aed8f621e05fac2bae124830ded
toan15042000 <toanndhe140540@fpt.edu.vn>
2023-04-15 00:31:16 +1000

    Make static content of ssl private key return string

--- SpringBootAPI/HRMS_SWP490_G2/src/main/java/com/swp490_g2/hrms/controller/StaticContentController.java
+++ SpringBootAPI/HRMS_SWP490_G2/src/main/java/com/swp490_g2/hrms/controller/StaticContentController.java
@@ -4,23 +4,29 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RestController
 @RequestMapping("/.well-known/pki-validation")
 public class StaticContentController {
     @GetMapping(
-            value = "/DD9D69AD06FDEBD3DA9134BAEDC9C12C.txt",
-            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+            value = "/DD9D69AD06FDEBD3DA9134BAEDC9C12C.txt"
     )
-    public @ResponseBody byte[] getFile() throws IOException {
+    public ResponseEntity<String> getFile() throws IOException {
         Resource resource = new FileSystemResource("uploads/DD9D69AD06FDEBD3DA9134BAEDC9C12C.txt");
-        return IOUtils.toByteArray(resource.getInputStream());
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return ResponseEntity.ok(FileCopyUtils.copyToString(reader));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
