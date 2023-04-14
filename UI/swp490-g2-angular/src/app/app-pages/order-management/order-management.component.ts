41841fbd362df572f994a8fcef84670aa37e6f2f
luongnvhe130930 <luongnvhe130930@fpt.edu.vn>
2023-04-15 01:16:12 +0700

    Create table for order management

--- UI/swp490-g2-angular/src/app/app-pages/order-management/order-management.component.ts
+++ UI/swp490-g2-angular/src/app/app-pages/order-management/order-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map, tap } from 'rxjs';
+import { OrderClient } from 'src/app/ngswag/client';
 
 @Component({
   selector: 'app-order-management',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-management.component.scss']
 })
 export class OrderManagementComponent implements OnInit {
-
-  constructor() { }
+  orderProductDetail: any
+  totalPrice: number
+  constructor(private $orderClient: OrderClient) { }
 
   ngOnInit() {
+    this.$orderClient.getAllByRole('SELLER', { page: 0, size: 15 } as any).subscribe(res => {
+      this.orderProductDetail = res.content;
+      console.log(this.orderProductDetail);
+
+      this.orderProductDetail.forEach(order => {
+        const totalPriceForOrder = order.orderProductDetails.reduce((total, item) => total + item.price, 0);
+        console.log('total price for order:', totalPriceForOrder);
+      });
+    });
   }
 
 }
