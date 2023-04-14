e44a592f102aa373b5f70f5a44c5f731c78172e9
luongnvhe130930 <luongnvhe130930@fpt.edu.vn>
2023-04-15 01:16:12 +0700

    Create table for order management

--- UI/swp490-g2-angular/src/app/app-pages/order-management/order-management.module.ts
+++ UI/swp490-g2-angular/src/app/app-pages/order-management/order-management.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderManagementComponent } from './order-management.component';
 import { OrderManagementRoutingModule } from './order-management-routing.module';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 @NgModule({
   imports: [
     CommonModule,
-    OrderManagementRoutingModule
+    OrderManagementRoutingModule,
+    TableModule,
+    ButtonModule,
+    TagModule
   ],
   declarations: [OrderManagementComponent]
 })
