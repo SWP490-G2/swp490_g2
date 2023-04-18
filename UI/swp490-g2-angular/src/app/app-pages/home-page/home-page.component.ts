4847e06458980a6ffc97d3d7e148b927b25f6e25
luongnvhe130930 <luongnvhe130930@fpt.edu.vn>
2023-04-18 22:11:39 +1000

    Make home page carousel responsive

--- UI/swp490-g2-angular/src/app/app-pages/home-page/home-page.component.ts
+++ UI/swp490-g2-angular/src/app/app-pages/home-page/home-page.component.ts
@@ -28,7 +28,23 @@ export class HomePageComponent implements OnInit {
 
   restaurantCategories: RestaurantCategory[] = [];
   allCategories: ProductCategory[] = [];
-  responsiveOptions: any[];
+  responsiveOptions = [
+    {
+      breakpoint: "1400px",
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: "1220px",
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: "1100px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
 
   constructor(
     private $router: Router,
