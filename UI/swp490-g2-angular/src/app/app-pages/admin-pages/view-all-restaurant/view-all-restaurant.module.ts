1848a1c5e29c41b233a7de31d64dcfc6021d6bc0
khanhnqhe141470 <khanhnqhe141470@fpt.edu.vn>
2023-05-01 08:36:17 +1000

    Changed activate/deactivate button behaviours

--- UI/swp490-g2-angular/src/app/app-pages/admin-pages/view-all-restaurant/view-all-restaurant.module.ts
+++ UI/swp490-g2-angular/src/app/app-pages/admin-pages/view-all-restaurant/view-all-restaurant.module.ts
@@ -14,6 +14,7 @@ import { InputSwitchModule } from "primeng/inputswitch";
 import { ToggleButtonModule } from "primeng/togglebutton";
 import { CheckboxModule } from "primeng/checkbox";
 import { InputTextModule } from "primeng/inputtext";
+import { TooltipModule } from "primeng/tooltip";
 
 @NgModule({
   declarations: [ViewAllRestaurantComponent],
@@ -30,7 +31,8 @@ import { InputTextModule } from "primeng/inputtext";
     InputSwitchModule,
     ToggleButtonModule,
     CheckboxModule,
-    InputTextModule
+    InputTextModule,
+    TooltipModule,
   ],
 })
 export class ViewAllRestaurantModule {}
