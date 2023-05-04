2569a21ff8c53ce11a05838e03ccf4eef462cceb
khanhnqhe141470 <khanhnqhe141470@fpt.edu.vn>
2023-05-04 12:32:57 +1000

    Time-zone database fixed

--- SpringBootAPI/HRMS_SWP490_G2/src/main/java/com/swp490_g2/hrms/repositories/OrderRepository.java
+++ SpringBootAPI/HRMS_SWP490_G2/src/main/java/com/swp490_g2/hrms/repositories/OrderRepository.java
@@ -25,22 +25,22 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query(value = """
             SELECT count(distinct(o.orderId)) totalOrders,
                 sum(opd.price * opd.quantity) totalSales,
-                date_format(o.completedAt, "%d-%m-%Y")
-            	FROM `order` o
-            		inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
-            		inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
-                    inner join restaurant_product rp on rp.products_productId = opd.product_productId
-            	where o.orderStatus = "COMPLETED"
-            	    and rp.restaurant_restaurantId = :restaurantId
-            		and date(o.completedAt) in (
-            			date_sub(curdate(), interval 6 + 7 * :offset day),
-            			date_sub(curdate(), interval 5 + 7 * :offset day),
-                        date_sub(curdate(), interval 4 + 7 * :offset day),
-                        date_sub(curdate(), interval 3 + 7 * :offset day),
-                        date_sub(curdate(), interval 2 + 7 * :offset day),
-                        date_sub(curdate(), interval 1 + 7 * :offset day),
-            			date_sub(curdate(), interval 0 + 7 * :offset day))
-            	group by date_format(o.completedAt, "%d-%m-%Y")
+                date_format(convert_tz(o.completedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
+            FROM `order` o
+                inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
+                inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
+                inner join restaurant_product rp on rp.products_productId = opd.product_productId
+            where o.orderStatus = "COMPLETED"
+                and rp.restaurant_restaurantId = :restaurantId
+                and date(convert_tz(o.completedAt, "+00:00" ,"+07:00")) in (
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 6 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 5 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 4 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 3 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 2 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 1 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 0 + 7 * :offset day))
+            group by date_format(convert_tz(o.completedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
             ;
             """, nativeQuery = true)
     List<Object> getReportIncomeOverTimeByRestaurantIdByWeekAndByCompletedOrderStatus(Long restaurantId, int offset);
@@ -48,90 +48,94 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query(value = """
             SELECT count(distinct(o.orderId)) totalOrders,
                 sum(opd.price * opd.quantity) totalSales,
-                date_format(o.abortedAt, "%d-%m-%Y")
-            	FROM `order` o
-            		inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
-            		inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
-                    inner join restaurant_product rp on rp.products_productId = opd.product_productId
-            	where o.orderStatus = "ABORTED"
-            	    and rp.restaurant_restaurantId = :restaurantId
-            		and date(o.abortedAt) in (
-            			date_sub(curdate(), interval 6 + 7 * :offset day),
-            			date_sub(curdate(), interval 5 + 7 * :offset day),
-                        date_sub(curdate(), interval 4 + 7 * :offset day),
-                        date_sub(curdate(), interval 3 + 7 * :offset day),
-                        date_sub(curdate(), interval 2 + 7 * :offset day),
-                        date_sub(curdate(), interval 1 + 7 * :offset day),
-            			date_sub(curdate(), interval 0 + 7 * :offset day))
-            	group by date_format(o.abortedAt, "%d-%m-%Y")
+                date_format(convert_tz(o.abortedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
+            FROM `order` o
+                inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
+                inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
+                inner join restaurant_product rp on rp.products_productId = opd.product_productId
+            where o.orderStatus = "ABORTED"
+                and rp.restaurant_restaurantId = :restaurantId
+                and date(convert_tz(o.abortedAt, "+00:00" ,"+07:00")) in (
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 6 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 5 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 4 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 3 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 2 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 1 + 7 * :offset day),
+                    date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval 0 + 7 * :offset day))
+            group by date_format(convert_tz(o.abortedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
             ;
             """, nativeQuery = true)
     List<Object> getReportIncomeOverTimeByRestaurantIdByWeekAndByAbortedOrderStatus(Long restaurantId, int offset);
 
     @Query(value = """
             SELECT count(distinct(o.orderId)) totalOrders,
-            sum(opd.price * opd.quantity) totalSales,
-            date_format(o.completedAt, "%d-%m-%Y")
+            	sum(opd.price * opd.quantity) totalSales,
+            	date_format(convert_tz(o.completedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
             FROM `order` o
-                inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
-                inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
-                inner join restaurant_product rp on rp.products_productId = opd.product_productId
+            	inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
+            	inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
+            	inner join restaurant_product rp on rp.products_productId = opd.product_productId
             where o.orderStatus = "COMPLETED"
-                and rp.restaurant_restaurantId = :restaurantId
-                and date(o.completedAt) between date_format(date_sub(curdate(), interval :offset month), "%Y-%m-01")
-                    and last_day(date_sub(curdate(), interval :offset month))
-            group by date_format(o.completedAt, "%d-%m-%Y")
+            	and rp.restaurant_restaurantId = :restaurantId
+            	and date(convert_tz(o.completedAt, "+00:00" ,"+07:00"))
+            	    between date_format(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset month), "%Y-%m-01")
+            		    and last_day(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset month))
+            group by date_format(convert_tz(o.completedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
             ;
             """, nativeQuery = true)
     List<Object> getReportIncomeOverTimeByRestaurantIdByMonthAndCompletedOrderStatus(Long restaurantId, int offset);
 
     @Query(value = """
             SELECT count(distinct(o.orderId)) totalOrders,
-            sum(opd.price * opd.quantity) totalSales,
-            date_format(o.abortedAt, "%d-%m-%Y")
+            	sum(opd.price * opd.quantity) totalSales,
+            	date_format(convert_tz(o.abortedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
             FROM `order` o
-                inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
-                inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
-                inner join restaurant_product rp on rp.products_productId = opd.product_productId
+            	inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
+            	inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
+            	inner join restaurant_product rp on rp.products_productId = opd.product_productId
             where o.orderStatus = "ABORTED"
-                and rp.restaurant_restaurantId = :restaurantId
-                and date(o.abortedAt) between date_format(date_sub(curdate(), interval :offset month), "%Y-%m-01")
-                    and last_day(date_sub(curdate(), interval :offset month))
-            group by date_format(o.abortedAt, "%d-%m-%Y")
+            	and rp.restaurant_restaurantId = :restaurantId
+            	and date(convert_tz(o.abortedAt, "+00:00" ,"+07:00"))
+            	    between date_format(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset month), "%Y-%m-01")
+            		    and last_day(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset month))
+            group by date_format(convert_tz(o.abortedAt, "+00:00" ,"+07:00"), "%d-%m-%Y")
             ;
             """, nativeQuery = true)
     List<Object> getReportIncomeOverTimeByRestaurantIdByMonthAndAbortedOrderStatus(Long restaurantId, int offset);
 
     @Query(value = """
-             SELECT count(distinct(o.orderId)) totalOrders,
-            sum(opd.price * opd.quantity) totalSales,
-            date_format(o.completedAt, "%m-%Y")
+            SELECT count(distinct(o.orderId)) totalOrders,
+            	sum(opd.price * opd.quantity) totalSales,
+            	date_format(convert_tz(o.completedAt, "+00:00" ,"+07:00"), "%m-%Y")
             FROM `order` o
-                inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
-                inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
-                inner join restaurant_product rp on rp.products_productId = opd.product_productId
+            	inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
+            	inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
+            	inner join restaurant_product rp on rp.products_productId = opd.product_productId
             where o.orderStatus = "COMPLETED"
-                and rp.restaurant_restaurantId = :restaurantId
-                and date(o.modifiedAt) between date_format(date_sub(curdate(), interval :offset year), "%Y-01-01")
-                    and date_format(date_sub(curdate(), interval :offset year), "%Y-12-31")
-            group by date_format(o.completedAt, "%m-%Y")
+            	and rp.restaurant_restaurantId = :restaurantId
+            	and date(convert_tz(o.completedAt, "+00:00" ,"+07:00"))
+            	    between date_format(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset year), "%Y-01-01")
+            		    and date_format(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset year), "%Y-12-31")
+            group by date_format(convert_tz(o.completedAt, "+00:00" ,"+07:00"), "%m-%Y")
             ;
             """, nativeQuery = true)
     List<Object> getReportIncomeOverTimeByRestaurantIdByYearAndCompletedOrderStatus(Long restaurantId, int offset);
 
     @Query(value = """
-             SELECT count(distinct(o.orderId)) totalOrders,
-            sum(opd.price * opd.quantity) totalSales,
-            date_format(o.abortedAt, "%m-%Y")
+            SELECT count(distinct(o.orderId)) totalOrders,
+            	sum(opd.price * opd.quantity) totalSales,
+            	date_format(convert_tz(o.abortedAt, "+00:00" ,"+07:00"), "%m-%Y")
             FROM `order` o
-                inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
-                inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
-                inner join restaurant_product rp on rp.products_productId = opd.product_productId
+            	inner join order_order_product_detail oopd on oopd.order_orderId = o.orderId
+            	inner join order_product_detail opd on opd.orderProductDetailId = oopd.orderProductDetails_orderProductDetailId
+            	inner join restaurant_product rp on rp.products_productId = opd.product_productId
             where o.orderStatus = "ABORTED"
-                and rp.restaurant_restaurantId = :restaurantId
-                and date(o.modifiedAt) between date_format(date_sub(curdate(), interval :offset year), "%Y-01-01")
-                    and date_format(date_sub(curdate(), interval :offset year), "%Y-12-31")
-            group by date_format(o.abortedAt, "%m-%Y")
+            	and rp.restaurant_restaurantId = :restaurantId
+            	and date(convert_tz(o.abortedAt, "+00:00" ,"+07:00"))
+            	    between date_format(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset year), "%Y-01-01")
+            		    and date_format(date_sub(date(convert_tz(now(), "+00:00" ,"+07:00")), interval :offset year), "%Y-12-31")
+            group by date_format(convert_tz(o.abortedAt, "+00:00" ,"+07:00"), "%m-%Y")
             ;
             """, nativeQuery = true)
     List<Object> getReportIncomeOverTimeByRestaurantIdByYearAndAbortedOrderStatus(Long restaurantId, int offset);
